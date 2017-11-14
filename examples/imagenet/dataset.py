49fde79013cf3160215f3f989779dd38d571b0ff
Kaushalya <kau.mad@gmail.com>
2017-11-14 20:18:13 +0900

    fixed for tensorflow 1.4

--- examples/imagenet/dataset.py
+++ examples/imagenet/dataset.py
@@ -20,7 +20,7 @@ def process_image(img, scale, isotropic, crop, mean):
         new_shape = tf.to_int32((scale / min_length) * img_shape)
     else:
         new_shape = tf.stack([scale, scale])
-    img = tf.image.resize_images(img, [new_shape[0], new_shape[1]])
+    img = tf.image.resize_images(img, new_shape)
     # Center crop
     # Use the slice workaround until crop_to_bounding_box supports deferred tensor shapes
     # See: https://github.com/tensorflow/tensorflow/issues/521
@@ -126,7 +126,7 @@ class ImageProducer(object):
         if self.data_spec.expects_bgr:
             # Convert from RGB channel ordering to BGR
             # This matches, for instance, how OpenCV orders the channels.
-            img = tf.reverse(img, [False, False, True])
+            img = tf.reverse(img,[2])
         return img
 
     def process(self):
